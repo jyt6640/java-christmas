0b28f9e42b6c855be3427a36e244f5b3c266a4bf
jyt6640 <jyt6640@gmail.com>
2026-01-03 16:50:24 +0900

    feat: 방문 날짜 및 메뉴와 개수를 입력받는 view 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,23 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public String readVisitDay() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String value = readLine();
+        return value;
+    }
+
+    public String readMenuAndCount() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String value = readLine();
+        return value;
+    }
+
+    protected String readLine() {
+        return Console.readLine();
+    }
+
 }
